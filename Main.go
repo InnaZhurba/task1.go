15ed4a702e25e0be00af71ecb95f998b290849b8
innazhurba <inazhurba@gmail.com>
2023-10-31 12:51:30 +0200

    init

--- Main.go
+++ Main.go
@@ -11,8 +11,12 @@ func main() {
 	service2 := Service.NewService("Service2", 2)
 	service3 := Service.NewService("Service3", 3)
 	service4 := Service.NewService("Service4", 2)
+	service5 := Service.NewService("Service5", 1)
+	service6 := Service.NewService("Service6", 1)
+	service7 := Service.NewService("Service7", 5)
+	service8 := Service.NewService("Service8", 3)
 
-	healthChecker := Healthchecker.NewHealthChecker([]*Service.Service{service1, service2, service3, service4})
+	healthChecker := Healthchecker.NewHealthChecker([]*Service.Service{service1, service2, service3, service4, service5, service6, service7, service8})
 
 	for {
 		healthChecker.Check()
